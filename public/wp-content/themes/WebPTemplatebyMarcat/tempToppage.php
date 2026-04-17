9e8e34dd50299d39be6ae98579e094cdd74bd701
Naoki Soga <n.soga@sunplacom>
2026-04-17 18:47:31 +0900

    --

--- public/wp-content/themes/WebPTemplatebyMarcat/tempToppage.php
+++ public/wp-content/themes/WebPTemplatebyMarcat/tempToppage.php
@@ -8,5 +8,6 @@
 <?php get_template_part('include/common/header/header'); ?>
 <main class="mainIndex">
     <?php get_template_part('include/layouts/top/01_topFv'); ?>
+    <?php get_template_part('include/layouts/top/02_bnr'); ?>
 </main>
 <?php get_template_part('include/common/footer/footer'); ?>
