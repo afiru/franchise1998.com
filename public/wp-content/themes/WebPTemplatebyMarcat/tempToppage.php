f6b997eb83770dfd0167ee809540e129839de451
Naoki Soga <n.soga@sunplacom>
2026-04-20 09:34:46 +0900

    ---

--- public/wp-content/themes/WebPTemplatebyMarcat/tempToppage.php
+++ public/wp-content/themes/WebPTemplatebyMarcat/tempToppage.php
@@ -8,6 +8,25 @@
 <?php get_template_part('include/common/header/header'); ?>
 <main class="mainIndex">
     <?php get_template_part('include/layouts/top/01_topFv'); ?>
-    <?php get_template_part('include/layouts/top/02_bnr'); ?>
+    <?php get_template_part('include/layouts/top/02_topbnr'); ?>
+    <?php get_template_part('include/layouts/top/03_topTopics'); ?>
+    <?php get_template_part('include/layouts/top/04_topAbout'); ?>
+    <?php get_template_part('include/layouts/top/05_topMade'); ?>
+    <?php get_template_part('include/layouts/top/06_topKikkae'); ?>
+    <?php get_template_part('include/layouts/top/07_topCollection'); ?>
+    <?php get_template_part('include/layouts/top/08_topLimited'); ?>
+    <?php get_template_part('include/layouts/top/09_topSelect'); ?>
+    <?php get_template_part('include/layouts/top/10_topOnline'); ?>
+    <?php get_template_part('include/layouts/top/11_topPickUp'); ?>
+    <?php get_template_part('include/layouts/top/12_topNav'); ?>
+    <?php get_template_part('include/layouts/top/13_topInstagram'); ?>
+    <?php get_template_part('include/layouts/top/14_topGifts'); ?>
+    <?php get_template_part('include/layouts/top/15_topThink'); ?>
+    <?php get_template_part('include/layouts/top/16_topAccess'); ?>
+    <?php get_template_part('include/layouts/top/17_topContact'); ?>
 </main>
+<footer>
+    <?php get_template_part('include/layouts/top/18_topFooterTop'); ?>
+    <?php get_template_part('include/layouts/top/19_topFooterBtm'); ?>
+</footer>
 <?php get_template_part('include/common/footer/footer'); ?>
