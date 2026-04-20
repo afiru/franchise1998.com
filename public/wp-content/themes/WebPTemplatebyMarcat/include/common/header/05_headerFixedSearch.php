b4f8b452e36ea49789c180ececae352acd08c02a
Naoki Soga <n.soga@sunplacom>
2026-04-20 11:15:12 +0900

    ---

--- public/wp-content/themes/WebPTemplatebyMarcat/include/common/header/05_headerFixedSearch.php
+++ public/wp-content/themes/WebPTemplatebyMarcat/include/common/header/05_headerFixedSearch.php
@@ -78,22 +78,176 @@
                 </ul>
             </div>
         </div>
-    </div>
+
+        <div class="bg_F9F9F9 opeHeaderFixedSearch">
+            <div class="wapper opeHeaderFixedSearchWap">
+                <section class="secOpeHeaderFixedSearch">
+                    <h2 class="mincho cl_453C3C fw_500 txtset h2OpeHeaderFixedSearch">目的から選ぶ</h2>
+                    <ul class="ulOpeHeaderFixedSearch">
+                        <li class="liOpeHeaderFixedSearch">
+                            <a class="d_flex j_center ali_center btnOpeHeaderFixedSearch" href="<?php echo home_url('//'); ?>">
+                                <figure class="iconOpeHeaderFixedSearch">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 15 15" fill="none">
+                                        <g clip-path="url(#clip0_2525_51654)">
+                                            <path d="M15 0H0V15H15V0Z" fill="#F9F9F9" />
+                                            <path d="M1.38281 9.65156V5.10156H2.09281L4.13281 8.33156V5.10156H4.85281V9.65156H4.16281L2.09281 6.33156V9.65156H1.38281Z" fill="#EA670F" />
+                                            <path d="M5.72656 9.65156V5.10156H8.61656V5.80156H6.43656V6.98156H8.42656V7.68156H6.43656V8.95156H8.68656V9.65156H5.72656Z" fill="#EA670F" />
+                                            <path d="M9.99125 9.65156L9.03125 5.10156H9.76125L10.3713 8.29156L11.1512 5.10156H11.7913L12.5712 8.29156L13.1813 5.10156H13.9213L12.9613 9.65156H12.3113L11.4713 6.29156L10.6413 9.65156H9.99125Z" fill="#EA670F" />
+                                            <path d="M0.710938 13.5547H14.2909" stroke="#EA670F" stroke-linecap="round" stroke-linejoin="round" />
+                                            <path d="M0.710938 1.45312H14.2909" stroke="#EA670F" stroke-linecap="round" stroke-linejoin="round" />
+                                        </g>
+                                        <defs>
+                                            <clipPath id="clip0_2525_51654">
+                                                <rect width="15" height="15" fill="white" />
+                                            </clipPath>
+                                        </defs>
+                                    </svg>
+                                </figure>
+                                <p class="cl_453C3C fw_500 txtset txtOpeHeaderFixedSearch">新商品・期間限定</p>
+                            </a>
+                        </li>
+
+                        <li class="liOpeHeaderFixedSearch">
+                            <a class="d_flex j_center ali_center btnOpeHeaderFixedSearch" href="<?php echo home_url('//'); ?>">
+                                <figure class="iconOpeHeaderFixedSearch">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="13" height="16" viewBox="0 0 13 16" fill="none">
+                                        <path d="M1.20312 12.5V15H11.7914V12.5" stroke="#EA670F" stroke-linecap="round" stroke-linejoin="round" />
+                                        <path d="M6.49805 6.07141V2.5" stroke="#EA670F" stroke-linecap="round" stroke-linejoin="round" />
+                                        <path d="M10.0273 6.07141V2.5" stroke="#EA670F" stroke-linecap="round" stroke-linejoin="round" />
+                                        <path d="M2.9707 6.07141V2.5" stroke="#EA670F" stroke-linecap="round" stroke-linejoin="round" />
+                                        <ellipse cx="10.0301" cy="0.714283" rx="0.705882" ry="0.714283" fill="#EA670F" />
+                                        <ellipse cx="6.50276" cy="0.714283" rx="0.705882" ry="0.714283" fill="#EA670F" />
+                                        <ellipse cx="2.96565" cy="0.714283" rx="0.705882" ry="0.714283" fill="#EA670F" />
+                                        <path d="M0.5 10.3585C0.874058 10.358 1.24314 10.2717 1.5794 10.1059C1.91566 9.94007 2.21033 9.69917 2.44118 9.40134C2.67238 9.69851 2.96721 9.93872 3.30345 10.1039C3.63969 10.269 4.00859 10.3549 4.38235 10.3549C4.75612 10.3549 5.12501 10.269 5.46125 10.1039C5.7975 9.93872 6.09232 9.69851 6.32353 9.40134C6.57113 9.7193 6.89133 9.97166 7.25661 10.1367C7.62189 10.3018 8.02132 10.3746 8.42057 10.349C8.81981 10.3234 9.20694 10.2 9.54866 9.9895C9.89037 9.77902 10.1765 9.48771 10.3824 9.14062C10.6013 9.51145 10.9114 9.81855 11.2826 10.032C11.6537 10.2454 12.0731 10.3579 12.5 10.3585" stroke="#EA670F" stroke-linecap="round" stroke-linejoin="round" />
+                                        <path d="M0.5 10.356V7.14174C0.5 6.85758 0.611554 6.58506 0.810122 6.38413C1.00869 6.18319 1.27801 6.07031 1.55882 6.07031H11.4412C11.722 6.07031 11.9913 6.18319 12.1899 6.38413C12.3884 6.58506 12.5 6.85758 12.5 7.14174V10.356" stroke="#EA670F" stroke-linecap="round" stroke-linejoin="round" />
+                                    </svg>
+                                </figure>
+                                <p class="cl_453C3C fw_500 txtset txtOpeHeaderFixedSearch">誕生日・記念日</p>
+                            </a>
+                        </li>
 
 
-    <div class="bg_F9F9F9 opeHeaderFixedSearch">
-        <div class="wapper opeHeaderFixedSearchWap">
-            <section class="secOpeHeaderFixedSearch">
-                <h2 class="mincho cl_453C3C fw_500 txtset h2OpeHeaderFixedSearch">目的から選ぶ</h2>
-                <ul class="ulOpeHeaderFixedSearch">
-                    <li class="liOpeHeaderFixedSearch">
-                        <a class="d_flex j_center ali_center btnOpeHeaderFixedSearch" href="<?php echo home_url('//'); ?>">
-                            <figure class="iconOpeHeaderFixedSearch"></figure>
-                        </a>
-                    </li>
-                </ul>
-            </section>
+                        <li class="liOpeHeaderFixedSearch">
+                            <a class="d_flex j_center ali_center btnOpeHeaderFixedSearch" href="<?php echo home_url('//'); ?>">
+                                <figure class="iconOpeHeaderFixedSearch">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 15 15" fill="none">
+                                        <g clip-path="url(#clip0_2525_51665)">
+                                            <path d="M15 0H0V15H15V0Z" fill="#F9F9F9" />
+                                            <path d="M12.6394 7.98313V13.7731H2.35938V6.70312" stroke="#EA670F" stroke-linecap="round" stroke-linejoin="round" />
+                                            <path d="M8.78906 4.13281H13.6091V6.70281H8.78906" stroke="#EA670F" stroke-linecap="round" stroke-linejoin="round" />
+                                            <path d="M7.50063 6.70281H1.39062V4.13281H7.50063" stroke="#EA670F" stroke-linecap="round" stroke-linejoin="round" />
+                                            <path d="M7.5 13.7728V4.13281" stroke="#EA670F" stroke-linecap="round" stroke-linejoin="round" />
+                                            <path d="M7.5 4.13219C7.5 4.13219 7.5 1.24219 10.31 1.24219C11.87 1.24219 11.85 4.13219 10.31 4.13219" stroke="#EA670F" stroke-linecap="round" stroke-linejoin="round" />
+                                            <path d="M7.49882 4.13219C7.49882 4.13219 7.49882 1.24219 4.68882 1.24219C3.12882 1.24219 3.07882 4.13219 4.68882 4.13219" stroke="#EA670F" stroke-linecap="round" stroke-linejoin="round" />
+                                        </g>
+                                        <defs>
+                                            <clipPath id="clip0_2525_51665">
+                                                <rect width="15" height="15" fill="white" />
+                                            </clipPath>
+                                        </defs>
+                                    </svg>
+                                </figure>
+                                <p class="cl_453C3C fw_500 txtset txtOpeHeaderFixedSearch">ギフト</p>
+                            </a>
+                        </li>
+
+                        <li class="liOpeHeaderFixedSearch">
+                            <a class="d_flex j_center ali_center btnOpeHeaderFixedSearch" href="<?php echo home_url('//'); ?>">
+                                <figure class="iconOpeHeaderFixedSearch">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="15" height="16" viewBox="0 0 15 16" fill="none">
+                                        <g clip-path="url(#clip0_2525_51689)">
+                                            <path d="M15 0H0V15H15V0Z" fill="#F9F9F9" />
+                                            <path d="M8.64781 4.3425C8.8632 4.3425 9.03781 4.16789 9.03781 3.9525C9.03781 3.73711 8.8632 3.5625 8.64781 3.5625C8.43242 3.5625 8.25781 3.73711 8.25781 3.9525C8.25781 4.16789 8.43242 4.3425 8.64781 4.3425Z" fill="#EA670F" />
+                                            <path d="M5.85875 4.3425C6.07414 4.3425 6.24875 4.16789 6.24875 3.9525C6.24875 3.73711 6.07414 3.5625 5.85875 3.5625C5.64336 3.5625 5.46875 3.73711 5.46875 3.9525C5.46875 4.16789 5.64336 4.3425 5.85875 4.3425Z" fill="#EA670F" />
+                                            <path d="M10.3084 5.33906C10.5184 4.89906 10.6384 4.41906 10.6384 3.90906C10.6384 2.04906 9.12844 0.539062 7.26844 0.539062C5.40844 0.539062 3.89844 2.04906 3.89844 3.90906C3.89844 4.42906 4.01844 4.90906 4.22844 5.33906" stroke="#EA670F" stroke-linecap="round" stroke-linejoin="round" />
+                                            <path d="M6.92969 2.15906C7.05969 2.23906 7.20969 2.27906 7.36969 2.27906C7.84969 2.27906 8.23969 1.88906 8.23969 1.40906C8.23969 0.929062 7.84969 0.539062 7.36969 0.539062" stroke="#EA670F" stroke-linecap="round" stroke-linejoin="round" />
+                                            <path d="M10.9276 4.89844C10.9276 4.89844 12.3476 5.85844 11.6376 8.99844C11.6376 8.99844 11.0376 14.2284 7.27755 14.5684C3.51755 14.2384 2.91755 8.99844 2.91755 8.99844C2.20755 5.85844 3.62755 4.89844 3.62755 4.89844" stroke="#EA670F" stroke-linecap="round" stroke-linejoin="round" />
+                                            <path d="M4.625 6.29688C4.625 6.29688 7.975 8.30687 9.645 11.4969" stroke="#EA670F" stroke-linecap="round" stroke-linejoin="round" />
+                                            <path d="M7.00781 8.14687C7.00781 8.14687 8.93781 6.54688 9.98781 6.29688" stroke="#EA670F" stroke-linecap="round" stroke-linejoin="round" />
+                                            <path d="M7.00781 5.10156C7.00781 5.10156 7.29781 5.31156 7.54781 5.10156" stroke="#EA670F" stroke-linecap="round" stroke-linejoin="round" />
+                                        </g>
+                                        <defs>
+                                            <clipPath id="clip0_2525_51689">
+                                                <rect width="15" height="15.07" fill="white" />
+                                            </clipPath>
+                                        </defs>
+                                    </svg>
+                                </figure>
+                                <p class="cl_453C3C fw_500 txtset txtOpeHeaderFixedSearch">内祝い</p>
+                            </a>
+                        </li>
+
+                        <li class="liOpeHeaderFixedSearch">
+                            <a class="d_flex j_center ali_center btnOpeHeaderFixedSearch" href="<?php echo home_url('//'); ?>">
+                                <figure class="iconOpeHeaderFixedSearch">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 15 15" fill="none">
+                                        <g clip-path="url(#clip0_2525_51679)">
+                                            <path d="M15 0H0V15H15V0Z" fill="#F9F9F9" />
+                                            <path d="M9.63281 6.87656V2.47656" stroke="#EA670F" stroke-miterlimit="10" stroke-linecap="round" />
+                                            <path d="M7.50781 6.87937V0.859375" stroke="#EA670F" stroke-miterlimit="10" stroke-linecap="round" />
+                                            <path d="M5.375 6.87656V2.47656" stroke="#EA670F" stroke-miterlimit="10" stroke-linecap="round" />
+                                            <path d="M12.6637 9.03219V6.99219H2.34375V9.03219C2.34375 10.1022 2.83375 11.0622 3.60375 11.6922L3.25375 12.6522C3.09375 13.1022 3.32375 13.6022 3.77375 13.7722C4.22375 13.9322 4.72375 13.7022 4.89375 13.2522L5.19375 12.4222C5.38375 12.4522 5.56375 12.4722 5.76375 12.4722H6.42375V13.0822C6.42375 13.6722 6.90375 14.1522 7.49375 14.1522C8.08375 14.1522 8.56375 13.6722 8.56375 13.0822V12.4722H9.22375C9.41375 12.4722 9.60375 12.4522 9.79375 12.4222L10.0938 13.2522C10.2537 13.7022 10.7637 13.9422 11.2137 13.7722C11.6637 13.6122 11.9037 13.1022 11.7337 12.6522L11.3837 11.6922C12.1537 11.0622 12.6438 10.1122 12.6438 9.03219H12.6637Z" stroke="#EA670F" stroke-linecap="round" stroke-linejoin="round" />
+                                        </g>
+                                        <defs>
+                                            <clipPath id="clip0_2525_51679">
+                                                <rect width="15" height="15" fill="white" />
+                                            </clipPath>
+                                        </defs>
+                                    </svg>
+                                </figure>
+                                <p class="cl_453C3C fw_500 txtset txtOpeHeaderFixedSearch">仏事</p>
+                            </a>
+                        </li>
+
+                        <li class="liOpeHeaderFixedSearch">
+                            <a class="d_flex j_center ali_center btnOpeHeaderFixedSearch" href="<?php echo home_url('//'); ?>">
+                                <figure class="iconOpeHeaderFixedSearch">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 15 15" fill="none">
+                                        <g clip-path="url(#clip0_2525_51702)">
+                                            <path d="M15 0H0V15H15V0Z" fill="#F9F9F9" />
+                                            <path d="M7.49813 10.1681C8.97273 10.1681 10.1681 8.97273 10.1681 7.49813C10.1681 6.02352 8.97273 4.82812 7.49813 4.82812C6.02352 4.82812 4.82812 6.02352 4.82812 7.49813C4.82812 8.97273 6.02352 10.1681 7.49813 10.1681Z" stroke="#EA670F" stroke-miterlimit="10" />
+                                            <path d="M12.6399 7.49937C13.5899 6.67937 13.8999 5.30937 13.2499 4.17937C12.5999 3.04937 11.2599 2.62938 10.0699 3.04938C9.83993 1.81938 8.79993 0.859375 7.49993 0.859375C6.19993 0.859375 5.15993 1.80938 4.92993 3.04938C3.73993 2.63938 2.39993 3.04937 1.74993 4.17937C1.09993 5.30937 1.40993 6.67937 2.35993 7.49937C1.40993 8.31937 1.09993 9.68938 1.74993 10.8194C2.39993 11.9494 3.73993 12.3694 4.92993 11.9494C5.15993 13.1794 6.19993 14.1394 7.49993 14.1394C8.79993 14.1394 9.83993 13.1894 10.0699 11.9494C11.2599 12.3694 12.5999 11.9494 13.2499 10.8194C13.8999 9.68938 13.5899 8.31937 12.6399 7.49937Z" stroke="#EA670F" stroke-miterlimit="10" />
+                                        </g>
+                                        <defs>
+                                            <clipPath id="clip0_2525_51702">
+                                                <rect width="15" height="15" fill="white" />
+                                            </clipPath>
+                                        </defs>
+                                    </svg>
+                                </figure>
+                                <p class="cl_453C3C fw_500 txtset txtOpeHeaderFixedSearch">お花を贈る</p>
+                            </a>
+                        </li>
+                    </ul>
+                </section>
+            </div>
+        </div>
+
+        <div class="wapper ctaHeaderFixedSearch">
+            <ul class="d_flex j_between row ulCtaHeaderFixedSearch">
+                <li class="liCtaHeaderFixedSearch">
+                    <a class="d_flex j_center ali_center bg_fff cl_421D12 fw_500 txtset undernone btnCtaHeaderFixedSearch" href="<?php echo home_url(); ?>">
+                        商品一覧を見る
+                    </a>
+                </li>
+
+                <li class="liCtaHeaderFixedSearch">
+                    <a class="d_flex j_center ali_center bg_fff cl_421D12 fw_500 txtset undernone btnCtaHeaderFixedSearch" href="<?php echo home_url(); ?>">
+                        <span class="d_block t_center iconCtaHeaderFixedSearch">ONLINE SHOP</span>
+                        <!--bg:../img/iconCtaHeaderFixedSearch.svg--->
+                    </a>
+                </li>
+            </ul>
+
+            <div class="closeHeaderFixedSearch">
+                <div class="c_pointer d_flex j_center ali_center bg_421D12 cl_fff mincho btncloseHeaderFixedSearch">
+                    <span class="d_block t_center mincho iconBtncloseHeaderFixedSearch">とじる</span>
+                    <!--bg:../img/iconBtncloseHeaderFixedSearch.svg--->
+                </div>
+            </div>
         </div>
     </div>
+
+
 
 </div>
