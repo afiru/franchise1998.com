5f75622af5c9afe4e3785fb10e876ce5399a175c
Naoki Soga <n.soga@sunplacom>
2026-04-24 14:52:54 +0900

    ---

--- public/wp-content/themes/WebPTemplatebyMarcat/include/layouts/sitemap/02_sitemapMain.php
+++ public/wp-content/themes/WebPTemplatebyMarcat/include/layouts/sitemap/02_sitemapMain.php
@@ -5,89 +5,89 @@
                 <li class="liSitemapMain">
                     <a class="cl_453C3C fw_500 txtset undernone btnLiSitemapMain" href="<?php echo home_url('/'); ?>">TOP</a>
                 </li>
-                <li class="liSitemapMain">
-                    <a class="cl_453C3C fw_500 txtset undernone btnLiSitemapMain" href="<?php echo home_url('/'); ?>">フランシーズについて</a>
+                <li class="liSitemapMain ">
+                    <a class="cl_453C3C fw_500 txtset undernone btnLiSitemapMain" href="<?php echo home_url('/about/'); ?>">フランシーズについて</a>
                 </li>
-                <li class="liSitemapMain">
-                    <a class="cl_453C3C fw_500 txtset undernone btnLiSitemapMain" href="<?php echo home_url('/'); ?>">－ 商品の魅力</a>
+                <li class="liSitemapMain liSitemapMain02">
+                    <a class="cl_6C6262 fw_500 txtset undernone btnLiSitemapMain02" href="<?php echo home_url('/appeal/'); ?>">－ 商品の魅力</a>
                 </li>
-                <li class="liSitemapMain">
-                    <a class="cl_453C3C fw_500 txtset undernone btnLiSitemapMain" href="<?php echo home_url('/'); ?>">－ 素材へのこだわり</a>
+                <li class="liSitemapMain liSitemapMain02">
+                    <a class="cl_6C6262 fw_500 txtset undernone btnLiSitemapMain02" href="<?php echo home_url('/material/'); ?>">－ 素材へのこだわり</a>
                 </li>
-                <li class="liSitemapMain">
-                    <a class="cl_453C3C fw_500 txtset undernone btnLiSitemapMain" href="<?php echo home_url('/'); ?>">－ スタッフの想い</a>
-                </li>
-            </ul>
-
-            <ul class="ulSitemapMain">
-                <li class="liSitemapMain">
-                    <a class="cl_453C3C fw_500 txtset undernone btnLiSitemapMain" href="<?php echo home_url('/'); ?>">商品一覧</a>
-                </li>
-                <li class="liSitemapMain">
-                    <a class="cl_453C3C fw_500 txtset undernone btnLiSitemapMain" href="<?php echo home_url('/'); ?>">－ デコレーション</a>
-                </li>
-                <li class="liSitemapMain">
-                    <a class="cl_453C3C fw_500 txtset undernone btnLiSitemapMain" href="<?php echo home_url('/'); ?>">－ ケーキ</a>
-                </li>
-                <li class="liSitemapMain">
-                    <a class="cl_453C3C fw_500 txtset undernone btnLiSitemapMain" href="<?php echo home_url('/'); ?>">－ 焼き菓子・ギフト</a>
-                </li>
-                <li class="liSitemapMain">
-                    <a class="cl_453C3C fw_500 txtset undernone btnLiSitemapMain" href="<?php echo home_url('/'); ?>">－ オリジナルプリントスイーツ</a>
-                </li>
-                <li class="liSitemapMain">
-                    <a class="cl_453C3C fw_500 txtset undernone btnLiSitemapMain" href="<?php echo home_url('/'); ?>">－ たま卵チーズ</a>
-                </li>
-                <li class="liSitemapMain">
-                    <a class="cl_453C3C fw_500 txtset undernone btnLiSitemapMain" href="<?php echo home_url('/'); ?>">－ ウェディング</a>
-                </li>
-                <li class="liSitemapMain">
-                    <a class="cl_453C3C fw_500 txtset undernone btnLiSitemapMain" href="<?php echo home_url('/'); ?>">－ お花・アレンジメント</a>
+                <li class="liSitemapMain liSitemapMain02">
+                    <a class="cl_6C6262 fw_500 txtset undernone btnLiSitemapMain02" href="<?php echo home_url('/staff/'); ?>">－ スタッフの想い</a>
                 </li>
             </ul>
 
-            <ul class="ulSitemapMain">
+            <ul class="ulSitemapMain ulSitemapMain02">
                 <li class="liSitemapMain">
-                    <a class="cl_453C3C fw_500 txtset undernone btnLiSitemapMain" href="<?php echo home_url('/'); ?>">トピックス</a>
+                    <a class="cl_453C3C fw_500 txtset undernone btnLiSitemapMain" href="<?php echo home_url('/products/'); ?>">商品一覧</a>
                 </li>
                 <li class="liSitemapMain">
-                    <a class="cl_453C3C fw_500 txtset undernone btnLiSitemapMain" href="<?php echo home_url('/'); ?>">お祝い・ギフトのご提案</a>
+                    <a class="cl_6C6262 fw_500 txtset undernone btnLiSitemapMain" href="<?php echo home_url('/products/product-category/decoration/'); ?>">－ デコレーション</a>
                 </li>
                 <li class="liSitemapMain">
-                    <a class="cl_453C3C fw_500 txtset undernone btnLiSitemapMain" href="<?php echo home_url('/'); ?>">お買い物ガイド・よくある質問</a>
+                    <a class="cl_6C6262 fw_500 txtset undernone btnLiSitemapMain" href="<?php echo home_url('/products/product-category/cake/'); ?>">－ ケーキ</a>
                 </li>
                 <li class="liSitemapMain">
-                    <a class="cl_453C3C fw_500 txtset undernone btnLiSitemapMain" href="<?php echo home_url('/'); ?>">お客様の声</a>
+                    <a class="cl_6C6262 fw_500 txtset undernone btnLiSitemapMain" href="<?php echo home_url('/products/product-category/baked-goods-and-gifts/'); ?>">－ 焼き菓子・ギフト</a>
                 </li>
                 <li class="liSitemapMain">
-                    <a class="cl_453C3C fw_500 txtset undernone btnLiSitemapMain" href="<?php echo home_url('/'); ?>">店舗情報・アクセス</a>
+                    <a class="cl_6C6262 fw_500 txtset undernone btnLiSitemapMain" href="<?php echo home_url('/products/product-category/original-sweets/'); ?>">－ オリジナルプリントスイーツ</a>
+                </li>
+                <li class="liSitemapMain">
+                    <a class="cl_6C6262 fw_500 txtset undernone btnLiSitemapMain02" href="<?php echo home_url('/products/product-category/tama-egg-cheese/'); ?>">－ たま卵チーズ</a>
+                </li>
+                <li class="liSitemapMain">
+                    <a class="cl_6C6262 fw_500 txtset undernone btnLiSitemapMain02" href="<?php echo home_url('/products/product-category/wedding/'); ?>">－ ウェディング</a>
+                </li>
+                <li class="liSitemapMain">
+                    <a class="cl_6C6262 fw_500 txtset undernone btnLiSitemapMain02" href="<?php echo home_url('/products/product-category/flowers-and-arrangements/'); ?>">－ お花・アレンジメント</a>
+                </li>
+            </ul>
+
+            <ul class="ulSitemapMain ulSitemapMain02">
+                <li class="liSitemapMain">
+                    <a class="cl_453C3C fw_500 txtset undernone btnLiSitemapMain" href="<?php echo home_url('/topics/'); ?>">トピックス</a>
+                </li>
+                <li class="liSitemapMain">
+                    <a class="cl_453C3C fw_500 txtset undernone btnLiSitemapMain" href="<?php echo home_url('/suggestion/'); ?>">お祝い・ギフトのご提案</a>
+                </li>
+                <li class="liSitemapMain">
+                    <a class="cl_453C3C fw_500 txtset undernone btnLiSitemapMain" href="<?php echo home_url('/faq/'); ?>">お買い物ガイド・よくある質問</a>
+                </li>
+                <li class="liSitemapMain">
+                    <a class="cl_453C3C fw_500 txtset undernone btnLiSitemapMain" href="<?php echo home_url('/voice/'); ?>">お客様の声</a>
+                </li>
+                <li class="liSitemapMain">
+                    <a class="cl_453C3C fw_500 txtset undernone btnLiSitemapMain" href="<?php echo home_url('/access/'); ?>">店舗情報・アクセス</a>
                 </li>
                 <li class="liSitemapMain">
                     <a class="cl_453C3C fw_500 txtset undernone btnLiSitemapMain" href="<?php echo home_url('/'); ?>">営業日カレンダー</a>
                 </li>
                 <li class="liSitemapMain">
-                    <a class="cl_453C3C fw_500 txtset undernone btnLiSitemapMain" href="<?php echo home_url('/'); ?>">会員特典</a>
+                    <a class="cl_453C3C fw_500 txtset undernone btnLiSitemapMain" href="<?php echo home_url('/member/'); ?>">会員特典</a>
                 </li>
             </ul>
 
-            <ul class="ulSitemapMain">
+            <ul class="ulSitemapMain ulSitemapMain02">
                 <li class="liSitemapMain">
-                    <a class="cl_453C3C fw_500 txtset undernone btnLiSitemapMain" href="<?php echo home_url('/'); ?>">オリジナルプリントスイーツ</a>
+                    <a class="cl_453C3C fw_500 txtset undernone btnLiSitemapMain" href="<?php echo home_url('/printedsweets/'); ?>">オリジナルプリントスイーツ</a>
                 </li>
                 <li class="liSitemapMain">
-                    <a class="cl_453C3C fw_500 txtset undernone btnLiSitemapMain" href="<?php echo home_url('/'); ?>">たま卵チーズ</a>
+                    <a class="cl_453C3C fw_500 txtset undernone btnLiSitemapMain" href="<?php echo home_url('/tamaran/'); ?>">たま卵チーズ</a>
                 </li>
                 <li class="liSitemapMain">
-                    <a class="cl_453C3C fw_500 txtset undernone btnLiSitemapMain" href="<?php echo home_url('/'); ?>">パーティープラン</a>
+                    <a class="cl_453C3C fw_500 txtset undernone btnLiSitemapMain" href="<?php echo home_url('/partyplan/'); ?>">パーティープラン</a>
                 </li>
                 <li class="liSitemapMain">
-                    <a class="cl_453C3C fw_500 txtset undernone btnLiSitemapMain" href="<?php echo home_url('/'); ?>">ウェディングサービス</a>
+                    <a class="cl_453C3C fw_500 txtset undernone btnLiSitemapMain" href="<?php echo home_url('/wedding/'); ?>">ウェディングサービス</a>
                 </li>
                 <li class="liSitemapMain">
                     <a class="cl_453C3C fw_500 txtset undernone btnLiSitemapMain" href="<?php echo home_url('/'); ?>">採用情報</a>
                 </li>
                 <li class="liSitemapMain">
-                    <a class="cl_453C3C fw_500 txtset undernone btnLiSitemapMain" href="<?php echo home_url('/'); ?>">お問い合わせ</a>
+                    <a class="cl_453C3C fw_500 txtset undernone btnLiSitemapMain" href="<?php echo home_url('/contact/'); ?>">お問い合わせ</a>
                 </li>
                 <li class="liSitemapMain">
                     <a class="cl_453C3C fw_500 txtset undernone btnLiSitemapMain" href="<?php echo home_url('/'); ?>">ケーキWEB予約</a>
