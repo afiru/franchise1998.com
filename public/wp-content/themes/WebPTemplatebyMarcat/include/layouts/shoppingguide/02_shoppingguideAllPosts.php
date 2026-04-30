403de6b23781d5c438d81cfdccec82212a7a6ef8
Miho Kinoshita <kinoshita-k@sunpla.com>
2026-04-30 19:14:26 +0900

    --

--- public/wp-content/themes/WebPTemplatebyMarcat/include/layouts/shoppingguide/02_shoppingguideAllPosts.php
+++ public/wp-content/themes/WebPTemplatebyMarcat/include/layouts/shoppingguide/02_shoppingguideAllPosts.php
@@ -7,13 +7,13 @@ $terms = get_terms(array(
 ));
 ?>
 <?php if ($terms) : ?>
-    <div class="shoppingguideAllPosts">
-        <div class="wapper shoppingguideAllPostsWap">
-            <ul class="ulShoppingguideAllPosts">
-                <?php foreach ($terms as $cat) : ?>
-                    <li class="liShoppingguideAllPosts">
-                        <h3 class="cl_453C3C fw_500 txtset h3LiShoppingguideAllPosts"><?php echo esc_html($cat->name); ?></h3>
-                        <?php
+<div class="shoppingguideAllPosts">
+    <div class="wapper shoppingguideAllPostsWap">
+        <ul class="ulShoppingguideAllPosts">
+            <?php foreach ($terms as $cat) : ?>
+            <li class="liShoppingguideAllPosts">
+                <h3 class="cl_453C3C fw_500 txtset h3LiShoppingguideAllPosts"><?php echo esc_html($cat->name); ?></h3>
+                <?php
                         $query = new WP_Query(array(
                             'post_type'      => 'faqs',
                             'posts_per_page' => -1,
@@ -26,34 +26,34 @@ $terms = get_terms(array(
                             )
                         ));
                         ?>
-                        <?php if ($query->have_posts()) : ?>
-                            <ul class="sbShoppingguideAllPosts">
-                                <?php while ($query->have_posts()) : $query->the_post(); ?>
-                                    <li class="bg_F1ECE8 liSbShoppingguideAllPosts">
-                                        <div class="cntLiSbShoppingguideAllPosts">
-                                            <div class="d_flex j_between c_pointer titleCntLiSbShoppingguideAllPosts jstitleCntLiSbShoppingguideAllPosts off">
-                                                <h3 class="cl_453C3C kaku fw_500 txtset h3SbShoppingguideAllPosts">
-                                                    <?php echo get_the_title($post->ID); ?>
-                                                </h3>
-                                                <figure class="iconSbShoppingguideAllPosts">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" width="13" height="8" viewBox="0 0 13 8" fill="none">
-                                                        <path d="M0.355469 0.353516L6.35547 6.35352L12.3555 0.353516" stroke="#453C3C" />
-                                                    </svg>
-                                                </figure>
-                                            </div>
-                                            <div class="mainCntLiSbShoppingguideAllPostsLxn jsmainCntLiSbShoppingguideAllPostsLxn">
-                                                <div class="mainCntLiSbShoppingguideAllPosts">
-                                                    <?php the_content(); ?>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </li>
-                                <?php endwhile; ?>
-                            </ul>
-                        <?php endif; ?>
+                <?php if ($query->have_posts()) : ?>
+                <ul class="sbShoppingguideAllPosts">
+                    <?php while ($query->have_posts()) : $query->the_post(); ?>
+                    <li class="bg_F1ECE8 liSbShoppingguideAllPosts">
+                        <div class="cntLiSbShoppingguideAllPosts">
+                            <div class="d_flex j_between c_pointer titleCntLiSbShoppingguideAllPosts jstitleCntLiSbShoppingguideAllPosts off">
+                                <h3 class="cl_453C3C kaku fw_500 txtset h3SbShoppingguideAllPosts">
+                                    Q.<?php echo get_the_title($post->ID); ?>
+                                </h3>
+                                <figure class="iconSbShoppingguideAllPosts">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="13" height="8" viewBox="0 0 13 8" fill="none">
+                                        <path d="M0.355469 0.353516L6.35547 6.35352L12.3555 0.353516" stroke="#453C3C" />
+                                    </svg>
+                                </figure>
+                            </div>
+                            <div class="mainCntLiSbShoppingguideAllPostsLxn jsmainCntLiSbShoppingguideAllPostsLxn">
+                                <div class="mainCntLiSbShoppingguideAllPosts">
+                                    <?php the_content(); ?>
+                                </div>
+                            </div>
+                        </div>
                     </li>
-                <?php endforeach; ?>
-            </ul>
-        </div>
+                    <?php endwhile; ?>
+                </ul>
+                <?php endif; ?>
+            </li>
+            <?php endforeach; ?>
+        </ul>
     </div>
+</div>
 <?php endif; ?>
