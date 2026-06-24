7f0449e0fbe30f8b0221b46a762a8fa0d084d527
Naoki Soga <n.soga@sunplacom>
2026-06-24 18:06:02 +0900

    ---

--- public/wp-content/themes/WebPTemplatebyMarcat/include/layouts/partyPlan/07_partyPlanDrinkMenu.php
+++ public/wp-content/themes/WebPTemplatebyMarcat/include/layouts/partyPlan/07_partyPlanDrinkMenu.php
@@ -12,23 +12,44 @@
 
         <ul class="d_flex j_start ulPartyPlanDrinkMenu">
             <?php foreach (scf::get('loopDrinkMenu') as $fields): ?>
-            <li class="liPartyPlanDrinkMenu">
-                <div class="d_flex j_between row liPartyPlanDrinkMenuFx">
-                    <figure class="photoPartyPlanDrinkMenu">
-                        <?php $img = get_scf_img_loop_url_id($fields['imgsDrinkMenu']); ?>
-                        <img loading="lazy" src="<?php echo $img[0]; ?>" alt="<?php echo $fields['titleDrinkMenu']; ?>写真" width="<?php echo $img[1]; ?>" height="<?php echo $img[2]; ?>">
-                    </figure>
-                    <section class="secPartyPlanDrinkMenuFx">
-                        <h4 class="cl_772D2D fw_500 txtset h4PartyPlanDrinkMenu"><?php echo $fields['titleDrinkMenu']; ?></h4>
-                        <p class="cl_772D2D fw_600 txtset pricePartyPlanDrinkMenu"><?php echo $fields['priceDrinkMenu']; ?></p>
-                    </section>
-                </div>
-                <p class="cl_453C3C fw_400 txtset text_justify txtPartyPlanDrinkMenu">
-                    <?php echo nl2br($fields['txtdrinkmenu']); ?>
-                </p>
-            </li>
+                <li class="liPartyPlanDrinkMenu">
+                    <div class="d_flex j_between row liPartyPlanDrinkMenuFx">
+                        <figure class="photoPartyPlanDrinkMenu">
+                            <?php $img = get_scf_img_loop_url_id($fields['imgsDrinkMenu']); ?>
+                            <img loading="lazy" src="<?php echo $img[0]; ?>" alt="<?php echo $fields['titleDrinkMenu']; ?>写真" width="<?php echo $img[1]; ?>" height="<?php echo $img[2]; ?>">
+                        </figure>
+                        <section class="secPartyPlanDrinkMenuFx">
+                            <h4 class="cl_772D2D fw_500 txtset h4PartyPlanDrinkMenu"><?php echo $fields['titleDrinkMenu']; ?></h4>
+                            <p class="cl_772D2D fw_600 txtset pricePartyPlanDrinkMenu"><?php echo $fields['priceDrinkMenu']; ?></p>
+                        </section>
+                    </div>
+                    <p class="cl_453C3C fw_400 txtset text_justify txtPartyPlanDrinkMenu">
+                        <?php echo nl2br($fields['txtdrinkmenu']); ?>
+                    </p>
+                </li>
             <?php endforeach; ?>
+        </ul>
 
+        <ul class="d_flex j_start ulPartyPlanDrinkMenu">
+            <?php foreach (scf::get('loopNonAlcoholDrinkMenu') as $fields): ?>
+                <li class="liPartyPlanDrinkMenu">
+                    <div class="d_flex j_between row liPartyPlanDrinkMenuFx">
+                        <?php $img = get_scf_img_loop_url_id($fields['imgsNonAlcoholDrinkMenu']); ?>
+                        <?php if (!empty($img[0])): ?>
+                            <figure class="photoPartyPlanDrinkMenu">
+                                <img loading="lazy" src="<?php echo $img[0]; ?>" alt="<?php echo $fields['titleNonAlcoholDrinkMenu']; ?>写真" width="<?php echo $img[1]; ?>" height="<?php echo $img[2]; ?>">
+                            </figure>
+                        <?php endif; ?>
+                        <section class="secPartyPlanDrinkMenuFx secPartyPlanDrinkMenuFxNon">
+                            <h4 class="cl_772D2D fw_500 txtset h4PartyPlanDrinkMenu"><?php echo $fields['titleNonAlcoholDrinkMenu']; ?></h4>
+                            <p class="cl_772D2D fw_600 txtset pricePartyPlanDrinkMenu"><?php echo $fields['priceNonAlcoholDrinkMenu']; ?></p>
+                        </section>
+                    </div>
+                    <p class="cl_453C3C fw_400 txtset text_justify txtPartyPlanDrinkMenu">
+                        <?php echo nl2br($fields['txtNonAlcoholdrinkmenu']); ?>
+                    </p>
+                </li>
+            <?php endforeach; ?>
         </ul>
 
         <p class="cl_6C6262 fw_400 txtDetailDrinkMenuOther">
